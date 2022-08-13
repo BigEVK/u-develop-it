6261e1fd197dde86c3393a4e76999f7350ccc76b
Eric VanKirk <eric@ericvankirk.com>
2022-08-13 11:33:31 -0500

    GET all and single candidates

--- server.js
+++ server.js
@@ -28,17 +28,42 @@ const db = mysql.createConnection(
 //     });
 // });
 
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 // db.query(`SELECT * FROM candidates`, (err, rows) => {
 //     console.log(rows);
 // });
 
 //  GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 7`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+db.query(sql, params, (err, row) => {
+    if (err) {
+        res.status(400).json({ erroe: err.message });
+        return;
+    }
+    res.json({
+        message: 'success',
+        data: row
+    });
+   });
+});
 
 // Delete candidate
 // db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
